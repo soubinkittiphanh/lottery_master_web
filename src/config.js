4ffdad57018b4d2c9104d3c2fc81b972ac7380c9
kittiphansoubin@gmail.com <kittiphanhsoubin@gmail.com>
2023-01-20 11:21:28 +0700

    update host againann

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 // const apiDomain="https://jfillapiapp.herokuapp.com/";
 // const apiDomain="https://apiwebpakse.herokuapp.com/";
-const apiDomain="https://nodejsclusters-99563-0.cloudclusters.net/";
+const apiDomain="https://nodejsclusters-99563-0.cloudclusters.net/";//new api host
 // const apiDomain= 'https://lotteryapidev.herokuapp.com/';
 // const apiDomain="http://localhost:8080/";//localhost
 module.exports={
